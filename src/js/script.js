e126008e997044e23ca71907ae15bdff6174fd25
Andrei911 <andrey_gk911@mail.ru>
2020-10-31 22:23:43 +0300

    add modal window + JS

--- src/js/script.js
+++ src/js/script.js
@@ -37,25 +37,41 @@ $('.carusel_slider').slick({
 	]
 
 });
-$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+$('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() { /* открывает страницу табов */
 	$(this)
 		.addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
 		.closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
 });
 
-$('.catalog-item__link').each(function(i) {
+$('.catalog-item__link').each(function(i) { /* работа ссылки ПОДРОБНЕЕ*/
 	$(this).on('click',function(e) {
 		e.preventDefault();
 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 	})
 });
-$('.catalog-item__back').each(function(i) {
+$('.catalog-item__back').each(function(i) {/* работа ссылки НАЗАД*/
 	$(this).on('click',function(e) {
 		e.preventDefault();
 		$('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
 		$('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
 	})
+
+	/*модальные окна*/
+
+	$('[data-modal=consultation]').on('click', function(){ /*назначает работу модыльного окна при нажатии на заданную кнопку */
+		$('.overlay,#consultation').fadeIn('slow');
+	});
+	$('.modal__close').on('click',function(){ /* назначает закрытие модального окна при нажатии на крестик*/
+		$('.overlay,#consultation,#order,#thaks').fadeOut('slow');	
+	});
+
+	$('.button_mini').each(function(i){ /* функция позволяет считывать нужную строку при открытии модального окна*/
+		$(this).on('click',function(){
+			$('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+			$('.overlay,#order').fadeIn('slow');
+		});
+	});
 });
 
 
