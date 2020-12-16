f115212d9907cd680665f6bbe468654bc4327a13
Andrei911 <andrey_gk911@mail.ru>
2020-12-16 18:20:25 +0100

    save js

--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,45 @@
-console.log('hello world');
-alert ( "hello world" );
+$(document).ready(function(){
+
+    (function($) {
+        $(function() {
+        
+        $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+            $(this)
+            .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
+            .closest('div.container').find('div.catalog__content').removeClass('catalog__content_active').eq($(this).index()).addClass('catalog__content_active');
+        });
+        
+        });
+    })(jQuery);
+
+    //tabs 
+
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click' , function(e){
+                e.preventDefault();
+                $('.catalog-item__content').eq(i).toggleClass('catalog-item__content_active');
+                $('.catalog-item__list').eq(i).toggleClass('catalog-item__list_active');
+            });
+        });
+    }
+    toggleSlide('.catalog-item__link');
+    toggleSlide('.catalog-item__back');
+
+    //Modal windows
+
+    $('[data-modal=consultation]').on('click' , function () {
+        $ ('.overlay , #consultation').fadeIn('slow');
+    });
+    $('.modal__close').on('click' , function () {
+        $ ('.overlay , #consultation, #order , #thanks').fadeOut('slow');
+    });
+
+    $('.button_mini').each(function(i){
+        $(this).on('click' , function() {
+            $('#order .modal__descr').text($('.catalog-item__subtitle').eq(i).text());
+            $ ('.overlay , #order').fadeIn('slow');
+        });
+    });
+  
+});
